edb3b60da9c7202c0257808e07d803c0bdf6628d
Alexander Ivanov <saiv46@yandex.ru>
2022-09-01 22:23:56 +0800

    Add testing index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// TODO
+const Yggdrasil = require('./src')
+const Config = require('./src/utils/config')
+
+async function main () {
+  const config = await Config.generate()
+  config.Peers = [
+    'tcp://127.0.0.1:13337'
+  ]
+
+  const ygg = new Yggdrasil(config)
+}
+
+main()
